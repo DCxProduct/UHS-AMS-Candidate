4e5652e40aaa60d7bbf8cf8c981a13d5dcb94fb9
DevMasters-lab <viratanasithan67@gmail.com>
2026-05-30 00:18:43 +0700

    Update switchig language one-click toggle

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return redirect('/admin/login');
 });
+
+Route::get('/language/toggle', function () {
+    $currentLocale = session('locale', app()->getLocale());
+
+    $nextLocale = $currentLocale === 'km' ? 'en' : 'km';
+
+    session(['locale' => $nextLocale]);
+
+    app()->setLocale($nextLocale);
+
+    return back();
+})->name('language.toggle');
