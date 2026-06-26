97a250d6246764ca68770d7e771c7ea3af1f51c1
Sophea <ttsophea@gmail.com>
2026-06-26 13:40:39 +0700

    sync
    sync

---
+++ app/Filament/Pages/Sync.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use App\Filament\Concerns\AdminOnly;
+use Filament\Pages\Page;
+
+class Sync extends Page
+{
+    use AdminOnly;
+
+    protected static string|\BackedEnum|null $navigationIcon = 'heroicon-o-arrow-path';
+
+    protected static string|\UnitEnum|null $navigationGroup = 'ការកំណត់';
+
+    protected static ?int $navigationSort = 90;
+
+    protected static ?string $navigationLabel = 'Sync';
+
+    protected static ?string $slug = 'sync';
+
+    protected string $view = 'filament.pages.sync';
+
+    public function getTitle(): string
+    {
+        return 'Sync';
+    }
+}
