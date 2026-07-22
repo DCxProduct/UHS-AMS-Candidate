dff7c9c187ab0ac9d2fbef593dfe8e120daac7ab
DevMasters-lab <viratanasithan67@gmail.com>
2026-07-22 09:06:00 +0700

    Update favicon in forgot password

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ __('app.forgot_password') }}</title>
+    <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
+    <link rel="shortcut icon" href="{{ asset('favicon.png') }}">
 
     @vite(['resources/css/app.css'])
 
